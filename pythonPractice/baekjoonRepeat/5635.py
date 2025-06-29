35ada838805deed990a8eab348f19d3c44dd25a6
kimkh0422 <your_email@example.com>
2025-06-29 18:47:50 +0900

    문제 풀이: 백준 5635번 생일, 소요시간 5분

--- pythonPractice/baekjoonRepeat/5635.py
+++ pythonPractice/baekjoonRepeat/5635.py
@@ -7,11 +7,10 @@ result = []
 
 for _ in range(N):
     name, dd, mm, yyyy = input().split()
-    result.append((name,yyyy,mm,dd))
+    result.append((name, int(yyyy), int(mm), int(dd)))
 
-#나이가 적은 사람 출력이니 yyyy,mm,dd순으로 대소비교 해서 가장 큰걸 처음 출력 즉 오름차순 정렬했을때 가장 뒤에 오는 사람 이름이 나이가 적은 사람
-result.sort(key= lambda x: (x[1],x[2],x[3]))
+# 나이가 적은 사람은 생일이 가장 늦은 사람
+result.sort(key=lambda x: (x[1], x[2], x[3]))  # 생년월일 오름차순
 
-print(result[-1][0])
-print(result[0][0])
-
+print(result[-1][0])  
+print(result[0][0])   
