191371d9601d6cfb0cb1013569bb47208114fb1d
kimkh0422 <kkh042299@naver.com>
2025-07-04 19:03:12 +0900

    문제 풀이: 백준 1463번 1로 만들기

---
+++ pythonPractice/baekjoonRepeat/1463.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+count = [0]*(N+1)
+
+for i in range(2,N+1):
+    count[i] = count[i-1]+1
+
+    if i%3==0:
+        count[i] = min(count[i-1]+1,count[i//3]+1)
+    if i%2==0:
+        count[i] = min(count[i-1]+1,count[i//2]+1)
+
+print(count[N])
