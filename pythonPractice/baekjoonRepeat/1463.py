2b7a7e5f94aa4bbee7aba48e53be1ba3eb9f922b
kimkh0422 <kkh042299@naver.com>
2025-07-04 19:15:13 +0900

    문제 풀이: 백준 1463번 1로 만들기

--- pythonPractice/baekjoonRepeat/1463.py
+++ pythonPractice/baekjoonRepeat/1463.py
@@ -9,8 +9,8 @@ for i in range(2,N+1):
     count[i] = count[i-1]+1
 
     if i%3==0:
-        count[i] = min(count[i-1]+1,count[i//3]+1)
+        count[i] = min(count[i],count[i//3]+1)
     if i%2==0:
-        count[i] = min(count[i-1]+1,count[i//2]+1)
+        count[i] = min(count[i],count[i//2]+1)
 
 print(count[N])
