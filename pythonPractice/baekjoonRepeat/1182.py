98614e87354a263a4bbcbe5ca1312ba3c5b67e1c
kimkh0422 <kkh042299@naver.com>
2025-08-04 17:31:54 +0900

    문제 풀이: 백준 1182번 부분수열의 합

---
+++ pythonPractice/baekjoonRepeat/1182.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+N, S = map(int,input().split())
+numbers = list(map(int,input().split()))
+
+count=0
+for i in range(1,N+1):
+    for comb in combinations(numbers,i):
+        if sum(comb)==S:
+            count+=1
+
+print(count)
