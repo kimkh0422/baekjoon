1fb9172f5f57b5298767afa070b48824a286e74b
kimkh0422 <kkh042299@naver.com>
2025-08-05 16:55:18 +0900

    문제 풀이: 백준 2563번 색종이

--- pythonPractice/baekjoonPractice/2563.py
+++ pythonPractice/baekjoonPractice/2563.py
@@ -1,24 +1,17 @@
 N = int(input())
 
-result = N * 100  
-papers = []
-
+board = [[0]*100 for _ in range(100)]
 
 for _ in range(N):
     x, y = map(int, input().split())
-    papers.append((x, y))
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            board[i][j] = 1
 
-
-for i in range(N):
-    x1, y1 = papers[i]
-    for j in range(i + 1, N):
-        x2, y2 = papers[j]
-
-        
-        xLen = max(0, min(x1 + 10, x2 + 10) - max(x1, x2))
-        yLen = max(0, min(y1 + 10, y2 + 10) - max(y1, y2))
-
-        area_len = xLen * yLen
-        result -= area_len
+result = 0
+for i in range(100):
+    for j in range(100):
+        if board[i][j] == 1:
+            result += 1
 
 print(result)
